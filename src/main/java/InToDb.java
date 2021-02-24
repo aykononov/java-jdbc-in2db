d24cfeaa638dc7c3dd7b7da5e72e151d803030cd
aykononov <aykononov@yandex.ru>
2021-02-25 00:24:02 +0300

    24.02.2021

--- src/main/java/InToDb.java
+++ src/main/java/InToDb.java
@@ -1,18 +1,31 @@
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class InToDb {
 
     public static void main(String[] args) {
         System.out.println("Start in2db...");
         ConfIni confIni = new ConfIni();
+        Connection con = null;
+
         try {
-            System.out.println("Connection to DataBase...");
-            Connection con = DriverManager.getConnection(confIni.getUrl(), confIni.getUsername(), confIni.getPassword());
+            con = DriverManager.getConnection(confIni.getUrl(), confIni.getUsername(), confIni.getPassword());
+            if (con != null) {
+                System.out.println("Connected to the DataBase.");
+            }
+            //Statement stm = con.createStatement();
+            //ResultSet rsl = stm.executeQuery();
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
+        } finally {
+            if (con != null ) {
+                try {
+                    con.close();
+                    System.out.println("Сlose the connection.");
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
         }
     }
 }
